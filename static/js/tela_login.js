5889505e01d5164163a1ede3182df7ea62111177
Caio Lacerda <162323538+caioy2k@users.noreply.github.com>
2026-03-17 21:27:34 -0300

    Update tela_login.js

--- static/js/tela_login.js
+++ static/js/tela_login.js
@@ -1,4 +1,4 @@
-const API_URL = "https://bemaqui-tcc-main.onrender.com/";
+const API_URL = "https://bemahi.onrender.com";
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("login-form");
