9558ffa6f29dde882e5a37a3d99a642f4906fe3e
caioy2k <caio.araujolacerda10@gmail.com>
2026-03-24 20:18:55 -0300

    em processo

--- static/js/tela_carteira.js
+++ static/js/tela_carteira.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://bemaqui-tcc-main.onrender.com";
 
 async function loadWallet() {
   try {
--- static/js/tela_login.js
+++ static/js/tela_login.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://bemaqui-tcc-main.onrender.com";
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("login-form");
