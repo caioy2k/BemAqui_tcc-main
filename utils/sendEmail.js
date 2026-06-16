3f47b5abfded502c884dcf78ddc6f3948f952268
caioy2k <caio.araujolacerda10@gmail.com>
2026-06-15 23:02:53 -0300

    teste para achr erro

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const EMAIL_HOST = process.env.EMAIL_HOST;
-const EMAIL_PORT = Number(process.env.EMAIL_PORT || 465);
+const EMAIL_PORT = Number(process.env.EMAIL_PORT || 587);
 const EMAIL_USER = process.env.EMAIL_USER;
 const EMAIL_PASS = process.env.EMAIL_PASS;
 const EMAIL_FROM = process.env.EMAIL_FROM;
@@ -10,34 +10,32 @@ const transporter = nodemailer.createTransport({
   host: EMAIL_HOST,
   port: EMAIL_PORT,
   secure: EMAIL_PORT === 465,
+  requireTLS: EMAIL_PORT === 587,
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASS
   },
   connectionTimeout: 10000,
   greetingTimeout: 10000,
-  socketTimeout: 15000
+  socketTimeout: 15000,
+  logger: true,
+  debug: true
 });
 
 async function sendEmail({ to, subject, html, text }) {
-  if (!EMAIL_HOST || !EMAIL_PORT || !EMAIL_USER || !EMAIL_PASS || !EMAIL_FROM) {
+  if (!EMAIL_HOST || !EMAIL_USER || !EMAIL_PASS || !EMAIL_FROM) {
     throw new Error("Variáveis de e-mail não configuradas corretamente.");
   }
 
-  console.log("Verificando conexão SMTP...");
   await transporter.verify();
-  console.log("SMTP conectado com sucesso.");
 
-  const info = await transporter.sendMail({
+  return transporter.sendMail({
     from: EMAIL_FROM,
     to,
     subject,
     text,
     html
   });
-
-  console.log("E-mail enviado:", info.messageId);
-  return info;
 }
 
 module.exports = sendEmail;
